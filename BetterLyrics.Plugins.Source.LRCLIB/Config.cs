0e9f1d203478912c39909a704d3aaa725e32b65e
jayfunc <founchoo@outlook.com>
2026-06-30 21:47:36 +0800

    init code

---
+++ BetterLyrics.Plugins.Source.LRCLIB/Config.cs
@@ -0,0 +1,8 @@
+﻿using BetterLyrics.Sdk.Abstractions.Plugins;
+
+namespace BetterLyrics.Plugins.Source.LRCLIB
+{
+    public class Config : PluginConfigBase
+    {
+    }
+}
---
+++ BetterLyrics.Plugins.Source.LRCLIB/Serialization/SourceGenerationContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BetterLyrics.Plugins.Source.LRCLIB.Serialization
+{
+    [JsonSerializable(typeof(JsonElement))]
+    [JsonSourceGenerationOptions(WriteIndented = true)]
+    public partial class SourceGenerationContext : JsonSerializerContext { }
+}
